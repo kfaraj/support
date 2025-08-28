722d1e2cc5103a8301280acad4c7a33c439b4c85
Kamal Faraj <kfaraj.dev@gmail.com>
2025-08-28 19:08:13 +0200

    Migrate to OSSRH Staging API

--- buildSrc/src/main/kotlin/com/kfaraj/support/publish.gradle.kts
+++ buildSrc/src/main/kotlin/com/kfaraj/support/publish.gradle.kts
@@ -1,5 +1,10 @@
 package com.kfaraj.support
 
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.Base64
+
 plugins {
     `maven-publish`
     signing
@@ -45,8 +50,10 @@ publishing {
     }
     repositories {
         maven {
-            val stagingUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            val stagingUrl =
+                "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
+            val snapshotsUrl =
+                "https://central.sonatype.com/repository/maven-snapshots/"
             url = uri(if (version.toString().endsWith("-SNAPSHOT")) snapshotsUrl else stagingUrl)
             credentials {
                 username = properties["ossrhUsername"] as String?
@@ -58,4 +65,28 @@ publishing {
 
 signing {
     sign(publishing.publications["release"])
+}
+
+tasks.register("publishToMavenCentral") {
+    group = "publishing"
+    description = "Publishes all Maven publications produced by this project to Maven Central."
+    dependsOn("publish")
+    doLast {
+        val username = properties["ossrhUsername"] as String?
+        val password = properties["ossrhPassword"] as String?
+        val namespace = properties["ossrhNamespace"] as String?
+        val token = Base64.getEncoder().encodeToString("$username:$password".encodeToByteArray())
+        val client = HttpClient.newHttpClient()
+        val request = HttpRequest.newBuilder()
+            .uri(uri("https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/$namespace"))
+            .header("Authorization", "Bearer $token")
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        if (response.statusCode() < 400) {
+            logger.info(response.body())
+        } else {
+            logger.error(response.body())
+        }
+    }
 }
